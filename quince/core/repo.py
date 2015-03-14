32760652b9c0955c1e015c16043f86e0df6b4ba5
Kal Ahmed <kal@brightstardb.com>
2015-03-14 18:27:13 +0000

    Fix for escaping literals in quince nquad files

--- quince/core/repo.py
+++ quince/core/repo.py
@@ -183,7 +183,10 @@ class QuinceStore:
 
     @staticmethod
     def make_nquad(s, p, o, g):
-        return "{0} {1} {2} {3} .\n".format(s.n3(), p.n3(), o.n3(), g.n3())
+        if isinstance(o, rdflib.Literal):
+            return "{0} {1} {2} {3} .\n".format(s.n3(), p.n3(), _xmlcharref_encode(_quote_literal(o)), g.n3())
+        else:
+            return "{0} {1} {2} {3} .\n".format(s.n3(), p.n3(), o.n3(), g.n3())
 
     @staticmethod
     def make_nquad_pattern(s, p, o, g):
@@ -203,6 +206,48 @@ class QuinceStore:
         if isinstance(o, rdflib.BNode):
             o = o.skolemize()
         return s, p, o
+
+
+def _quote_literal(l):
+    """
+    Handles proper NQuads escaping of an rdflib Literal.
+    """
+    encoded = _quote_encode(l)
+    if l.language:
+        if l.datatype:
+            raise Exception("Literal has datatype AND language!")
+        return '%s@%s' % (encoded, l.language)
+    elif l.datatype:
+        return '%s^^<%s>' % (encoded, l.datatype)
+    else:
+        return '%s' % encoded
+
+
+def _quote_encode(l):
+    return '"%s"' % l.replace('\\', '\\\\')\
+        .replace('\n', '\\n')\
+        .replace('"', '\\"')\
+        .replace('\r', '\\r')
+
+
+# from <http://code.activestate.com/recipes/303668/>
+def _xmlcharref_encode(unicode_data, encoding="ascii"):
+    """Emulate Python 2.3's 'xmlcharrefreplace' encoding error handler."""
+    res = ""
+
+    # Step through the unicode_data string one character at a time in
+    # order to catch unencodable characters:
+    for char in unicode_data:
+        try:
+            char.encode(encoding, 'strict')
+        except UnicodeError:
+            if ord(char) <= 0xFFFF:
+                res += '\\u%04X' % ord(char)
+            else:
+                res += '\\U%08X' % ord(char)
+        else:
+            res += char
+    return res
 
 
 class SortedSet:
