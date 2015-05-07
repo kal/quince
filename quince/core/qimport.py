3dd5f5cd01b179818f9696fae92cba538bd5be90
Kal Ahmed <kal@brightstardb.com>
2015-05-07 15:46:56 +0100

    Added support for importing from a URL using urllib.

--- quince/core/qimport.py
+++ quince/core/qimport.py
@@ -1,6 +1,8 @@
 __author__ = 'Kal Ahmed'
 
 import os.path
+import urllib.request
+
 from quince.core.parsers import get_parser
 from quince.core.repo import qdir, QuinceStore, QuinceTripleSink, git_add_files
 from quince.core.exceptions import QuinceParseException, QuinceNoParserException
@@ -18,11 +20,14 @@ def import_file(file_path, default_graph=None):
         if os.path.isfile(file_path):
             with open(file_path, 'rb') as f:
                 parser.parse(f)
-            store.flush()
-            git_add_files()
-            return SUCCESS
+        elif file_path.lower().startswith('http://') or file_path.lower().startswith('https://'):
+            with urllib.request.urlopen(file_path) as f:
+                parser.parse(f)
         else:
             raise IOError("File not found")
+        store.flush()
+        git_add_files()
+        return SUCCESS
     except Exception as e:
         raise QuinceParseException(file_path, e)
 
